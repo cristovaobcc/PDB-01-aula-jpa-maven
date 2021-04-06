79a2369be374e84e464732c7357083ba871fa14d
cristovaobcc <cristovao.bcc@gmail.com>
2021-04-06 11:02:11 -0300

    Teste de inserção e busca de objetos no DB.

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -9,26 +9,39 @@ import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
-		
+
 		Pessoa p1 = new Pessoa(null, "Charles", "chl@email.com");
 		Pessoa p2 = new Pessoa(null, "Carlitos", "ca@email.com");
 		Pessoa p3 = new Pessoa(null, "Chaplin", "ch@email.com");
-		
+
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
+
+		//criaInstanciasNoDB(emf, em, p1, p2, p3);
 		
-		em.getTransaction().begin();
+		buscaPessoa(emf, em);
 		
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		
-		em.getTransaction().commit();
 		em.close();
 		emf.close();
-		
+
 		System.out.println("Pronto!");
-		
+
 	}
 
+	private static void criaInstanciasNoDB(EntityManagerFactory emf, EntityManager em, Pessoa... pessoas) {
+		em.getTransaction().begin();
+
+		for (int i = 0; i < pessoas.length; i++) {
+			em.persist(pessoas[i]);
+			
+		}
+		
+		em.getTransaction().commit();
+	}
+	
+	private static void buscaPessoa(EntityManagerFactory emf, EntityManager em) {
+		Pessoa p = em.find(Pessoa.class, 2);
+		
+		System.out.println(p);
+	}
 }
