f7d31d488f70ac567bae649ccfe47b6ca635b576
cristovaobcc <cristovao.bcc@gmail.com>
2021-04-06 10:27:17 -0300

    Adiciona anotações JPA na entidade e modifica o programa
    Após executar o Programa.java, verifique a inserção de dados no DB.

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,18 +1,32 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1, "Charles", "chl@email.com");
-		Pessoa p2 = new Pessoa(2, "Carlitos", "ca@email.com");
-		Pessoa p3 = new Pessoa(3, "Chaplin", "ch@email.com");
+		Pessoa p1 = new Pessoa(null, "Charles", "chl@email.com");
+		Pessoa p2 = new Pessoa(null, "Carlitos", "ca@email.com");
+		Pessoa p3 = new Pessoa(null, "Chaplin", "ch@email.com");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		
+		em.getTransaction().commit();
+		
+		System.out.println("Pronto!");
+		
 	}
 
 }
